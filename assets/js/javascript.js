baa7158dcebc7f719f961f49f1b1f8b5e460a70b
Jamie Lipschitz <jlipschitz@GENERALVISION.LOCAL>
2016-02-23 18:36:12 -0500

    wireframe

---
+++ assets/js/javascript.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+
+var game = {
+	obiWan {
+		name:"Obi-wan",
+		attack: 8,
+		health: 140,
+		counter: 5
+	}
+	yoda {
+		name: "Yoda",
+		attack: 10,
+		health: 120,
+		counter: 4
+	}
+	maceWindu {
+		name:"Mace Windu",
+		attack: 7,
+		health:150,
+		counter:7
+	}
+	snoke {
+		name: "Snoke",
+		attack:4
+		health: 170
+		counter: 8
+	}
+};
+
+function battle(playerAttack, playerHealth, enemyHealth, enemyCounter) {
+	
+}
+
+
+
+});
