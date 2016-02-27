463bfef923811e9be5333a508abfb58a2725cf90
Jamie Lipschitz <jlipschitz@GENERALVISION.LOCAL>
2016-02-27 17:58:32 -0500

    switched to constructor as base

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,36 +1,46 @@
-$( document ).ready(function() {
+    function Create(Name, Attack, Health, Counter, Url) {
+        this.name = Name;
+        this.attack = Attack;
+        this.health = Health;
+        this.counter = Counter;
+        this.imageUrl = Url;
+    };
 
-var game = {
-	obiWan {
-		name:"Obi-wan",
-		attack: 8,
-		health: 140,
-		counter: 5
-	}
-	yoda {
-		name: "Yoda",
-		attack: 10,
-		health: 120,
-		counter: 4
-	}
-	maceWindu {
-		name:"Mace Windu",
-		attack: 7,
-		health:150,
-		counter:7
-	}
-	snoke {
-		name: "Snoke",
-		attack:4
-		health: 170
-		counter: 8
-	}
-};
+    var characters = [yoda = new Create("Yoda", 10, 120, 5, "assets/images/yoda.png"),
+        obi = new Create("Obi-Wan", 5, 140, 6, "../assets/images/obi.png"),
+        vader = new Create("Darth Vader", 7, 150, 3, "../assets/images/darth.png"),
+        snoke = new Create("Snoke", 4, 160, 10, "../assets/images/snoke.png")
+    ];
 
-function battle(playerAttack, playerHealth, enemyHealth, enemyCounter) {
-	
-}
+    console.log(yoda.name);
+    console.log(characters[0].imageUrl);
 
+    function fight(hero, enemy) {
+        hero.health -= enemy.counter;
+        enemy.health -= hero.attack;
+        var messages = {
+            heroHit: enemy.name + " hit you for " + enemy.counter + "!" + " Health remaining: " + hero.health,
+            enemyHit: "You hit " + enemy.name + " for " + hero.attack + "! " + enemy.name + "'s health is now: " + enemy.health,
+            heroLose: enemy.name + " has defeated you!",
+            heroWin: hero.name + " has defeated " + enemy.name + "!"
+        };
+        console.log(messages.heroHit);
+        console.log(messages.enemyHit);
+        hero.attack += 3;
 
+        if (enemy.health <= 0) {
+            console.log(messages.heroWin);
+        } else if (hero.health <= 0) {
+            console.log(messages.heroLose);
+        }
+    };
 
-});
+    var starCharacters = function() {
+        var playerDivs = $("<div>");
+
+        for (var i = 0; i < characters.length; i++) {
+            //playerDivs[i].addClass("initial");
+            //$(".initialCharacters").append(playerDivs[i]);
+        }
+    };
+    starCharacters();
